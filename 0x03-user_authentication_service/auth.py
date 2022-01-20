cc0f7da6e2237dc11da58458a55439993d9db561
grillzwitu <grillzwitu@gmail.com>
2022-01-20 13:02:15 +0100

    implemented method to get user from session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -100,3 +100,20 @@ class Auth:
             return session_id
         except (NoResultFound, ValueError):
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """
+        Gets the user from a session id.
+        Args:
+            session_id: The session id.
+        Returns:
+            User: The user object
+            or None.
+        """
+        if not session_id:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
