1ff3dd1c35e0fa3e55745c514761a862ed917699
grillzwitu <grillzwitu@gmail.com>
2022-01-20 05:27:13 +0100

    refactored find_user_by method, __init__

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -17,7 +17,8 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db",
+                                     connect_args={"check_same_thread": False})
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -53,6 +54,8 @@ class DB:
         Returns:
             User: The found user or error
         """
+        if not kwargs:
+            raise InvalidRequestError
         user = self._session.query(User).filter_by(**kwargs).first()
         if not user:
             raise NoResultFound("No user found")
