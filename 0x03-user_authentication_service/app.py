8b694f9d325b669ba45f271c23f0ebad58b948c9
grillzwitu <grillzwitu@gmail.com>
2022-01-20 11:20:32 +0100

    implemented user registration

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,12 @@
 """
 Basic Flask app.
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+from typing import Union
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -15,6 +18,34 @@ def base() -> str:
         str: json payload
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def new_user() -> Union[str, tuple]:
+    """
+    POST method /users route
+    Registers new users with email and password,
+    or checks if the email is already registered
+    Return:
+      - json payload
+    """
+
+    # Get data from form request,
+    # convert to json with request.get_json() for the body
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        new_user = AUTH.register_user(email, password)
+        if new_user is not None:
+            return jsonify({
+                "email": new_user.email,
+                "message": "user created"
+            })
+    except ValueError:
+        return jsonify({
+            "message": "email already registered"
+            }), 400
 
 
 if __name__ == "__main__":
