3351b662f8e1072f6b24717f829847cab77dceb5
grillzwitu <grillzwitu@gmail.com>
2022-01-21 03:03:09 +0100

    implemented get reset token method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -128,3 +128,21 @@ class Auth:
             self._db.update_user(user_id, session_id=None)
         except ValueError:
             pass
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Gets a reset password token.
+        Args:
+            Email: The user's email.
+        Raises:
+            ValueError: If the user does not exist.
+        Returns:
+            str: The reset password token.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=token)
+        return token
